5ef70ed3f1fd88b3d687fc8b586ff6dbbde18c18
AmarnathCJD <amarnathcharichilil@gmail.com>
2021-12-04 14:43:43 +0530

    learned go basics

--- main.go
+++ main.go
@@ -1,17 +1,44 @@
 package main
 
-import ("fmt"
-    "github.com/PaulSonOfLars/gotgbot/v2", "os", "net/http")
+import (
+	"log"
+	"net/http"
+	"time"
+
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+var (
+	menu = &tb.ReplyMarkup{}
+)
 
 func main() {
-    fmt.Println("lel")
-    bot, err := gotgbot.NewBot(
-		os.Getenv("TOKEN"),
-		&gotgbot.BotOpts{
-			Client:      http.Client{},
-			GetTimeout:  gotgbot.DefaultGetTimeout,
-			PostTimeout: gotgbot.DefaultPostTimeout,
+	b, err := tb.NewBot(tb.Settings{
+		URL:       "",
+		Token:     "5050904599:AAGsihkB9Xj2UzPRZdcd2Mh4UVscSBiRPXQ",
+		Updates:   0,
+		Poller:    &tb.LongPoller{Timeout: 10 * time.Second},
+		ParseMode: "HTML",
+		Reporter: func(error) {
 		},
-	)
-    fmt.Println("done")
+		Client: &http.Client{},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	b.Handle("/start", func(m *tb.Message) {
+		if m.Private() {
+			menu.Inline(
+				menu.Row(menu.URL("Support", "t.me/roseloverx_support"), menu.URL("Updates", "t.me/roseloverx_support")),
+				menu.Row(menu.Data("Commands", "help_menu")),
+				menu.Row(menu.URL("Add me to your group", "https://t.me/Yoko_Robot?startgroup=true")))
+			b.Send(m.Sender, "Hey there! I am <b>Yoko</b>.\nIm an Anime themed Group Management Bot, feel free to add me to your groups!", menu)
+			return
+		}
+		b.Reply(m, "Hey I'm Alive.")
+	})
+
+	b.Start()
 }
