54cef3afadbcbb9d63bb7ae1efc6a46f4e82751b
Amarnath C <41010786@sfcollege.edu>
2021-12-05 18:41:36 +0530

    Update main.go

--- main.go
+++ main.go
@@ -21,15 +21,6 @@ var (
 	})
 )
 
-func Shellout(command string) (string, string) {
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
-	cmd := exec.Command("bash", "-c", command)
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-	err := cmd.Run()
-	return stdout.String(), err.Error()
-}
 
 func main() {
 	if err != nil {
