a068d9f766fb961cf6bab5657ac9b29d27feb344
Amarnath C <41010786@sfcollege.edu>
2021-12-07 19:00:27 +0530

    Update misc.go

--- misc.go
+++ misc.go
@@ -99,15 +99,22 @@ func getJson(url string) (mapType, error) {
 }
 
 func info(m *tb.Message) {
-        if !m.IsReply(); string(m.Payload) == string(""){
-          user_obj := m.Sender
+        if !m.IsReply(){
+          if string(m.Payload) == string(""){
+            user_obj := m.Sender
+            final_msg := fmt.Sprintf("<b>User info</b>\n<b>ID:</b> <code>%s</code>\n<b>First Name:</b> %s\n<b>Last Name:</b> %s\n<b>Username:</b> @%s\n<b>User Link:</b> <a href='tg://user?id=%s'>%s</a>\n\n<b>Gbanned:</b> %s", strconv.Itoa(int(user_obj.ID)), user_obj.FirstName, user_obj.LastName, user_obj.Username, strconv.Itoa(int(user_obj.ID)), "link", "No")
+	    _, err := b.Reply(m, final_msg)
+	    if err != nil {
+		fmt.Println(err)
+	    }
+          }
         } else {
           user_obj, _ := get_user(m)
-        }
-	final_msg := fmt.Sprintf("<b>User info</b>\n<b>ID:</b> <code>%s</code>\n<b>First Name:</b> %s\n<b>Last Name:</b> %s\n<b>Username:</b> @%s\n<b>User Link:</b> <a href='tg://user?id=%s'>%s</a>\n\n<b>Gbanned:</b> %s", strconv.Itoa(int(user_obj.ID)), user_obj.FirstName, user_obj.LastName, user_obj.Username, strconv.Itoa(int(user_obj.ID)), "link", "No")
-	_, err := b.Reply(m, final_msg)
-	if err != nil {
+          final_msg := fmt.Sprintf("<b>User info</b>\n<b>ID:</b> <code>%s</code>\n<b>First Name:</b> %s\n<b>Last Name:</b> %s\n<b>Username:</b> @%s\n<b>User Link:</b> <a href='tg://user?id=%s'>%s</a>\n\n<b>Gbanned:</b> %s", strconv.Itoa(int(user_obj.ID)), user_obj.FirstName, user_obj.LastName, user_obj.Username, strconv.Itoa(int(user_obj.ID)), "link", "No")
+      	  _, err := b.Reply(m, final_msg)
+	  if err != nil {
 		fmt.Println(err)
-	}
+	  }
+        }
 }
 
