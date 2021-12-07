f914e507e173926589b646c4b89307ebf9ae28d0
Amarnath C <41010786@sfcollege.edu>
2021-12-07 19:19:00 +0530

    Update misc.go

--- misc.go
+++ misc.go
@@ -102,12 +102,12 @@ func info(m *tb.Message) {
         if !m.IsReply() && string(m.Payload) == string(""){
             user_obj := m.Sender
             final_msg := fmt.Sprintf("<b>User info</b>\n<b>ID:</b> <code>%s</code>\n<b>First Name:</b> %s\n<b>Last Name:</b> %s\n<b>Username:</b> @%s\n<b>User Link:</b> <a href='tg://user?id=%s'>%s</a>\n\n<b>Gbanned:</b> %s", strconv.Itoa(int(user_obj.ID)), user_obj.FirstName, user_obj.LastName, user_obj.Username, strconv.Itoa(int(user_obj.ID)), "link", "No")
-	    _, err := b.Reply(m, final_msg)
+	    b.Reply(m, final_msg)
         } else {
           user_obj, _ := get_user(m)
           final_msg := fmt.Sprintf("<b>User info</b>\n<b>ID:</b> <code>%s</code>\n<b>First Name:</b> %s\n<b>Last Name:</b> %s\n<b>Username:</b> @%s\n<b>User Link:</b> <a href='tg://user?id=%s'>%s</a>\n\n<b>Gbanned:</b> %s", strconv.Itoa(int(user_obj.ID)), user_obj.FirstName, user_obj.LastName, user_obj.Username, strconv.Itoa(int(user_obj.ID)), "link", "No")
           p, _ := b.ProfilePhotosOf(user_obj)
-      	  _, err := b.Reply(m, final_msg, p[0])
+      	  b.Reply(m, final_msg, p[0])
         }
 }
 
