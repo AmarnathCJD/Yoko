57445e8f02a2778b58e1060736053362785302b0
Amarnath C <41010786@sfcollege.edu>
2021-12-09 18:08:41 +0530

    Update db.go

--- db.go
+++ db.go
@@ -1,2 +1,11 @@
 package main
 
+import (
+        "context"
+        "go.mongodb.org/mongo-driver/mongo"
+        "go.mongodb.org/mongo-driver/mongo/options"
+        "go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+db, _ := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://go:amar0245@lon5-c12-1.mongo.objectrocket.com:43391,lon5-c12-2.mongo.objectrocket.com:43391,lon5-c12-0.mongo.objectrocket.com:43391/go?replicaSet=24e1adf7f54a48fba7350c36009da162"))
+
