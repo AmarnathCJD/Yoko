1ae745c46d5d002c1adcafe659b156ee228c30b1
Amarnath C <41010786@sfcollege.edu>
2021-12-09 18:43:23 +0530

    Update locks.go

--- locks.go
+++ locks.go
@@ -3,6 +3,7 @@ package main
 import (
  tb "gopkg.in/tucnak/telebot.v2"
  "fmt"
+ "strings"
 )
 
 var LOCK_TYPES = [31]string{"all","album","audio", "bot", "button",  "command", "comment", "contact", "document",  "email",  "emojigame",  "forward",  "forwardbot", "forwardchannel","forwarduser","game", "gif",  "inline", "invitelink",  "location",  "phone",  "photo", "poll", "rtl",  "sticker", "text", "url", "video", "videonote",  "voice", "anonchannel"}
@@ -11,5 +12,10 @@ func lock(m *tb.Message){
  if m.Payload == string(""){
     b.Reply(m, "You haven't specified a type to lock.")
  }
- fmt.Println(LOCK_TYPES)
+ lk = strings.Split(m.Payload, " ")
+ for lock := range lk {
+     if stringInSlice(lock, LOCK_TYPE){
+        fmt.Println(lock)
+     }
+ }
 }
