0db1514dd9d9170d110642f7d9b58b46f6e9ebed
Amarnath C <41010786@sfcollege.edu>
2021-12-09 18:19:42 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"log"
-
+        "context"
+        "go.mongodb.org/mongo-driver/mongo"
+        "go.mongodb.org/mongo-driver/mongo/options"
+        "go.mongodb.org/mongo-driver/mongo/readpref"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
@@ -21,10 +24,8 @@ var (
 
 
 func main() {
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
+        dataBase, _ := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://go:amar0245@lon5-c12-1.mongo.objectrocket.com:43391,lon5-c12-2.mongo.objectrocket.com:43391,lon5-c12-0.mongo.objectrocket.com:43391/go?replicaSet=24e1adf7f54a48fba7350c36009da162"))
+        var db = dataBase
 	b.Handle("/eval", evaluate)
 	b.Handle("/start", func(m *tb.Message) {
 		if m.Private() {
