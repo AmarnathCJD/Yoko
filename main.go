a873fa0ffa74322fc3f43d3f5e52eed0e65dcc0c
Amarnath C <41010786@sfcollege.edu>
2021-12-09 18:21:49 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"log"
         "context"
         "go.mongodb.org/mongo-driver/mongo"
         "go.mongodb.org/mongo-driver/mongo/options"
-        "go.mongodb.org/mongo-driver/mongo/readpref"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
 var (
 	menu = &tb.ReplyMarkup{}
+        db, _ := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://go:amar0245@lon5-c12-1.mongo.objectrocket.com:43391,lon5-c12-2.mongo.objectrocket.com:43391,lon5-c12-0.mongo.objectrocket.com:43391/go?replicaSet=24e1adf7f54a48fba7350c36009da162"))
 )
 
 var (
@@ -24,8 +23,6 @@ var (
 
 
 func main() {
-        dataBase, _ := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://go:amar0245@lon5-c12-1.mongo.objectrocket.com:43391,lon5-c12-2.mongo.objectrocket.com:43391,lon5-c12-0.mongo.objectrocket.com:43391/go?replicaSet=24e1adf7f54a48fba7350c36009da162"))
-        var db = dataBase
 	b.Handle("/eval", evaluate)
 	b.Handle("/start", func(m *tb.Message) {
 		if m.Private() {
@@ -44,6 +41,6 @@ func main() {
         b.Handle("/gp", gp)
         b.Handle(tb.OnChatMember, greet_member)
         b.Handle("/imdb", IMDb)
-        b.Handel("/lock", lock)
+        b.Handle("/lock", lock)
 	b.Start()
 }
