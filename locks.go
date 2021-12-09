2eb2e82d434bdcbdeec1efa7290dd5a113afcadb
Amarnath C <41010786@sfcollege.edu>
2021-12-09 20:12:21 +0530

    Update locks.go

--- locks.go
+++ locks.go
@@ -15,19 +15,23 @@ func lock(m *tb.Message){
  }
  args := strings.Split(m.Payload, " ")
  to_lock := make([]string, 0)
+ locked_msg := ""
  for _, lock := range args {
      if stringInSlice(lock, LOCK_TYPES){
         to_lock = append(to_lock, lock)
+        locked_msg += fmt.Sprintf("\n- <code>%s</code>", lock)
      }
  }
  if len(to_lock) == 0{
     b.Reply(m, fmt.Sprintf("✨ Unknown lock types:- %s\nCheck <code>/locktypes</code>!", m.Payload))
     return
  }
- b.Reply(m, "Locked " + strings.Join(to_lock, "\n- "))
+ b.Reply(m, "Locked " + locked_msg)
 }
 
 func locktypes(m *tb.Message){
- lock_types := strings.Join(LOCK_TYPES,"\n<b>~</b> ")
+ lock_types := ""
+ for _, lock := range LOCK_TYPES {
+     lock_types += fmt.Sprintf("\n<b>~</b> %s", lock)
  b.Reply(m, "The available lock types are:" + lock_types)
 }
