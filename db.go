124072dcc03ec510c1a5cb600f3af3c289a4aaba
Amarnath C <41010786@sfcollege.edu>
2021-12-09 18:15:22 +0530

    Update db.go

--- db.go
+++ db.go
@@ -7,5 +7,9 @@ import (
         "go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-db, _ := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://go:amar0245@lon5-c12-1.mongo.objectrocket.com:43391,lon5-c12-2.mongo.objectrocket.com:43391,lon5-c12-0.mongo.objectrocket.com:43391/go?replicaSet=24e1adf7f54a48fba7350c36009da162"))
+func init_db() {
+ db, _ := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://go:amar0245@lon5-c12-1.mongo.objectrocket.com:43391,lon5-c12-2.mongo.objectrocket.com:43391,lon5-c12-0.mongo.objectrocket.com:43391/go?replicaSet=24e1adf7f54a48fba7350c36009da162"))
+ var db = db
+}
 
+init_db()
