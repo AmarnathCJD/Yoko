a014f7cbdb4d075c17fbe6afc3894c9049bda292
Amarnath C <41010786@sfcollege.edu>
2021-12-10 21:18:48 +0530

    Update db.go

--- db.go
+++ db.go
@@ -9,20 +9,19 @@ import (
 
 var (
 	database = db.Database("go")
-	locks_db = database.Collection("locks")
+	locks_db = database.Collection("locks_db")
 )
 
 func lock_item(chat_id int64, item string) bool {
 	filter := bson.M{"chat_id": chat_id}
 	locked := locks_db.FindOne(context.TODO(), filter)
 	if locked.Err() != nil {
-		fmt.Printf("locked.Err(): %v\n", locked.Err())
-		lock := bson.D{{"chat_id", chat_id}, {"locks", "meaw"}}
+		lock := bson.D{{"chat_id", chat_id}, {"locks", []string{item}}}
 		xd, err := locks_db.InsertOne(context.TODO(), lock)
-		fmt.Println(xd)
-		fmt.Printf("err.Error(): %v\n", err.Error())
 	} else {
-		fmt.Printf("locked: %v\n", locked)
+                var lock_list bson.M
+		locks := locked.Decode(&lock_list)
+                fmt.Println(locks)
 	}
 	return true
 }
