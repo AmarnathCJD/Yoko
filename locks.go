4d07b3f8bf8c1b0f13169f9ce319c9eaa8ba4cc5
Amarnath C <41010786@sfcollege.edu>
2021-12-11 11:46:12 +0530

    Update locks.go

--- locks.go
+++ locks.go
@@ -62,31 +62,31 @@ func check_locks(m *tb.Message) {
 
 func unlock(m *tb.Message) {
         if m.Payload == string("") {
-		b.Reply(m, "You haven't specified a type to lock.")
+		b.Reply(m, "You haven't specified a type to unlock.")
 		return
 	}
 	args := strings.Split(m.Payload, " ")
-	to_lock := make([]string, 0)
+	to_unlock := make([]string, 0)
         if stringInSlice("all", args){
-           b.Reply(m, "Locked <code>all</code>.")
-           lock_item(m.Chat.ID, LOCK_TYPES)
+           b.Reply(m, "Unlocked all.")
+           unlock_item(m.Chat.ID, LOCK_TYPES)
            return
         }
-	locked_msg := ""
+	unlocked_msg := ""
 	for _, lock := range args {
 		if stringInSlice(lock, LOCK_TYPES) {
-			to_lock = append(to_lock, lock)
-			locked_msg += fmt.Sprintf("\n- <code>%s</code>", lock)
+			to_unlock = append(to_unlock, lock)
+			unlocked_msg += fmt.Sprintf("\n- <code>%s</code>", lock)
 		}
 	}
-	if len(to_lock) == 0 {
+	if len(to_unlock) == 0 {
 		b.Reply(m, fmt.Sprintf("✨ Unknown lock types:- %s\nCheck /locktypes !", m.Payload))
 		return
 	}
-        if len(to_lock) == 1{
-          b.Reply(m, fmt.Sprintf("Locked <code>%s</code>.", to_lock[0]))
+        if len(to_unlock) == 1{
+          b.Reply(m, fmt.Sprintf("Unlocked <code>%s</code>.", to_unlock[0]))
           return
         }
-	b.Reply(m, "Locked "+locked_msg)
-        lock_item(m.Chat.ID, to_lock)
+	b.Reply(m, "Unlocked "+locked_msg)
+        unlock_item(m.Chat.ID, to_unlock)
 }
