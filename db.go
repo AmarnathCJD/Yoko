f6aebe82ddba6986ae394e37e342206da3fbdb3c
Amarnath C <41010786@sfcollege.edu>
2021-12-11 11:52:34 +0530

    Update db.go

--- db.go
+++ db.go
@@ -19,6 +19,15 @@ func isTrue(a string, list bson.A) bool {
 	return false
 }
 
+func remove(s bson.A, r string) bson.A {
+    for i, v := range s {
+        if v == r {
+            return append(s[:i], s[i+1:]...)
+        }
+    }
+    return s
+}
+
 func lock_item(chat_id int64, items []string) bool {
 	filter := bson.M{"chat_id": chat_id}
 	locked := locks_db.FindOne(context.TODO(), filter)
@@ -31,6 +40,23 @@ func lock_item(chat_id int64, items []string) bool {
                 new_lock := lock_list["locks"].(bson.A)
                 for _, lock := range items{
                    new_lock = append(new_lock, lock)
+                }
+                locks_db.UpdateOne(context.TODO(), filter, bson.D{{"$set", bson.D{{"locks", new_lock}}}})
+	}
+	return true
+}
+
+func unlock_item(chat_id int64, items []string) bool {
+	filter := bson.M{"chat_id": chat_id}
+	locked := locks_db.FindOne(context.TODO(), filter)
+	if locked.Err() != nil {
+		return false
+	} else {
+                var lock_list bson.M
+		locked.Decode(&lock_list)
+                new_lock := lock_list["locks"].(bson.A)
+                for _, lock := range items{
+                   new_lock = remove(new_lock, lock)
                 }
                 locks_db.UpdateOne(context.TODO(), filter, bson.D{{"$set", bson.D{{"locks", new_lock}}}})
 	}
