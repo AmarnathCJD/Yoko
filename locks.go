a7b35577d24849d6631ebacf1f63404af9d4742c
Amarnath C <41010786@sfcollege.edu>
2021-12-11 10:13:07 +0530

    Update locks.go

--- locks.go
+++ locks.go
@@ -53,13 +53,13 @@ func locktypes(m *tb.Message) {
 func check_locks(m *tb.Message) {
  locked := "✨ Chat LockSettings"
  lock_c := get_locks(m.Chat.ID)
- fmt.Println(lock_c)
  for _, lock := range LOCK_TYPES {
      if isTrue(lock, lock_c){
-        locked += fmt.Sprintf("\n<b>-/b> %s: true", lock)
+        locked += fmt.Sprintf("\n<b>-›</b> %s: true", lock)
      } else {
-        locked += fmt.Sprintf("\n<b>-</b> %s: false", lock)
+        locked += fmt.Sprintf("\n<b>-›</b> %s: false", lock)
      }
  }
+ fmt.Println(locked)
  b.Reply(m, locked)
 }
