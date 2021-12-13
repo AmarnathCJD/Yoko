77733bc4ab792d00ce85240af92dde79d0eedf58
Amarnath C <41010786@sfcollege.edu>
2021-12-13 21:19:45 +0530

    Update main.go

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ var (
 	b, _ = tb.NewBot(tb.Settings{
 		URL:         "",
 		Token:       "5050904599:AAEqSus8jD5vTVZzQeTVo4cBc8IwsRha_I0",
-		Poller:      &tb.LongPoller{Timeout: 10},
+		Poller:      &tb.LongPoller{Timeout: 10, AllowedUpdates: []string{"message", "chat_member"}},
 		Synchronous: false,
 		Verbose:     false,
 		ParseMode:   "HTML",
