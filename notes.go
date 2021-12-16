100d5f2416ec3899ceee8b335b00881645137276
Amarnath C <41010786@sfcollege.edu>
2021-12-16 07:18:55 +0530

    Update notes.go

--- notes.go
+++ notes.go
@@ -24,55 +24,48 @@ func save(c tb.Context) error {
 	return nil
 }
 
-func rgtest(c tb.Context) error {
+func all_notes(c tb.Context) error {
 	m := c.Message()
-	fmt.Println("hello")
-	b.Reply(m, m.Payload)
+	notes := get_notes(c.Chat().ID)
+	if notes == nil {
+		b.Reply(m, fmt.Sprintf("There are no notes in %s!", m.Chat.Title))
+		return nil
+	}
+	note := fmt.Sprintf("Notes in <b>%s</b>", c.Chat().Title)
+	for _, x := range notes {
+		note += fmt.Sprintf("\n<b>-&gt;</b> <code>%s</code>", x.(bson.M)["name"].(string))
+	}
+	b.Reply(m, note+"\nYou can retrieve these notes by using <code>/get notename</code>")
 	return nil
 }
 
-func all_notes(c tb.Context) error {
- m := c.Message()
- notes := get_notes(c.Chat().ID)
- if notes == nil{
-    b.Reply(m, fmt.Sprintf("There are no notes in %s!", m.Chat.Title))
-    return nil
- }
- note := fmt.Sprintf("Notes in <b>%s</b>", c.Chat().Title)
- for _, x := range notes{
-  note += fmt.Sprintf("\n<b>-&gt;</b> <code>%s</code>", x.(bson.M)["name"].(string))
- }
- b.Reply(m, note + "\nYou can retrieve these notes by using <code>/get notename</code>")
- return nil
-}
-
 func gnote(c tb.Context) error {
- m := c.Message()
- note := get_note(m.Chat.ID, m.Payload)
- if note == nil{
-    b.Reply(m, "No note found!")
-    return nil
- }
- unparse_message(note["file"], note["note"].(string), m)
- return nil
+	m := c.Message()
+	note := get_note(m.Chat.ID, m.Payload)
+	if note == nil {
+		b.Reply(m, "No note found!")
+		return nil
+	}
+	unparse_message(note["file"], note["note"].(string), m)
+	return nil
 }
  
 func hash_regex(next tb.HandlerFunc) tb.HandlerFunc {
-  return func(c tb.Context) error {
-    match, _ := regexp.MatchString("\\#(\\S+)", c.Message().Text)
-    if match{
-       return next(c)
-    }
-    return nil
-  }
+	return func(c tb.Context) error {
+		match, _ := regexp.MatchString("\\#(\\S+)", c.Message().Text)
+		if match {
+			return next(c)
+		}
+		return nil
+	}
 }
 
 func hash_note(c tb.Context) error {
- args := strings.SplitN(c.Message().Text, "#", 2)
- note := get_note(c.Message().Chat.ID, args[1])
- if note == nil{
-    return nil
- }
- unparse_message(note["file"], note["note"].(string), c.Message())
- return nil
+	args := strings.SplitN(c.Message().Text, "#", 2)
+	note := get_note(c.Message().Chat.ID, args[1])
+	if note == nil {
+		return nil
+	}
+	unparse_message(note["file"], note["note"].(string), c.Message())
+	return nil
 }
