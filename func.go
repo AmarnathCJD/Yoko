8d91838a2546315d610c593360b69b44a055a010
Amarnath C <41010786@sfcollege.edu>
2021-12-16 21:22:39 +0530

    Update func.go

--- func.go
+++ func.go
@@ -67,7 +67,7 @@ func get_file(m *tb.Message) (string, string) {
 
 func unparse_message(file interface{}, note string, m *tb.Message) {
         text, buttons := button_parser(note)
-	if file != nil && len(file.(bson.A)) != 0 {
+	if file != nil{
                 fmt.Println(6)
 		id, f := file.(bson.A)[0].(string), file.(bson.A)[1].(string)
 		if f == "document" {
@@ -134,7 +134,7 @@ func button_parser(text string) (string, *tb.ReplyMarkup) {
 	}
         btns.Inline(rows...)
         note := BTN_URL_REGEX.Split(text, -1)[0]
-        fmt.Println(btns)
+        fmt.Println(note)
         return note, btns
 }
 
