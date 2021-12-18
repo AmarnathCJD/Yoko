649a8708fe4c132277f0269eb86641cb372cf08b
Amarnath C <41010786@sfcollege.edu>
2021-12-18 20:38:11 +0530

    Update feds.go

--- feds.go
+++ feds.go
@@ -2,11 +2,19 @@ package main
 
 import (
  tb "gopkg.in/tucnak/telebot.v3
+ "fmt"
 )
 
 func new_fed(c tb.Context) error {
+ m := c.Message()
  if !c.Message().Private(){
-   b.Reply(c.Message(), "Create your federation in my PM - not in a group.")
+   b.Reply(m, "Create your federation in my PM - not in a group.")
    return nil
  }
+ fed := get_fed_by_owner(m.Sender.ID)
+ if fed[0] {
+    c.Reply(fmt.Sprintf("You already have a federation called <code>%s</code> ; you can't create another. If you would like to rename it, use <code>/renamefed</code>.", fed[2]))
+    return nil
+ }
  return nil
+}
