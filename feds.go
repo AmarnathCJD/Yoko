935ceb79630f7ba25ff17bf3efb21284c2fc094c
Amarnath C <41010786@sfcollege.edu>
2021-12-18 07:28:37 +0530

    Create feds.go

---
+++ feds.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+ tb "gopkg.in/tucnak/telebot.v3
+)
+
+func new_fed(c tb.Context) error {
+ if !c.Message.Private(){
+   b.Reply(c.Message(), "Create your federation in my PM - not in a group.")
+   return nil
+ }
+ return nil
