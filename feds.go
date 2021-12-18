1d19b885eb682c3c6baa39c1fbe9a8d77eb95ae9
Amarnath C <41010786@sfcollege.edu>
2021-12-18 20:57:06 +0530

    Update feds.go

--- feds.go
+++ feds.go
@@ -1,30 +1,31 @@
 package main
 
 import (
- tb "gopkg.in/tucnak/telebot.v3"
- "fmt"
+	"fmt"
+
+	tb "gopkg.in/tucnak/telebot.v3"
 )
 
 func new_fed(c tb.Context) error {
- m := c.Message()
- if !c.Message().Private(){
-   b.Reply(m, "Create your federation in my PM - not in a group.")
-   return nil
- }
- fed, _, fedname := get_fed_by_owner(m.Sender.ID)
- if fed {
-    c.Reply(fmt.Sprintf("You already have a federation called <code>%s</code> ; you can't create another. If you would like to rename it, use <code>/renamefed</code>.", fedname))
-    return nil
- }
- fed_name := m.Payload
- if fed_name == string("") {
-    c.Reply("You need to give your federation a name! Federation names can be up to 64 characters long.")
-    return nil
- } else if len(fed_name) > 64 {
-    c.Reply("Federation names can only be upto 64 charactors long.")
-    return nil
- }
- fed_uid, _ := make_new_fed(m.Sender.ID, fed_name)
- c.Reply(fmt.Sprintf("Created new federation with FedID: <code>%s</code>.\nUse this ID to join the federation! eg:\n<code>/joinfed %s</code>", fed_uid, fed_uid))
- return nil
+	m := c.Message()
+	if !c.Message().Private() {
+		b.Reply(m, "Create your federation in my PM - not in a group.")
+		return nil
+	}
+	fed, _, fedname := get_fed_by_owner(m.Sender.ID)
+	if fed {
+		c.Reply(fmt.Sprintf("You already have a federation called <code>%s</code> ; you can't create another. If you would like to rename it, use <code>/renamefed</code>.", fedname))
+		return nil
+	}
+	fed_name := m.Payload
+	if fed_name == string("") {
+		c.Reply("You need to give your federation a name! Federation names can be up to 64 characters long.")
+		return nil
+	} else if len(fed_name) > 64 {
+		c.Reply("Federation names can only be upto 64 charactors long.")
+		return nil
+	}
+	fed_uid, _ := make_new_fed(m.Sender.ID, fed_name)
+	c.Reply(fmt.Sprintf("Created new federation with FedID: <code>%s</code>.\nUse this ID to join the federation! eg:\n<code>/joinfed %s</code>", fed_uid, fed_uid))
+	return nil
 }
