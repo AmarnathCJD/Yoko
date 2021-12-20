66752c791fb46bb6e13e871bf87738770de38088
Amarnath C <41010786@sfcollege.edu>
2021-12-20 21:25:23 +0530

    Update misc.go

--- modules/misc.go
+++ modules/misc.go
@@ -190,7 +190,7 @@ func Bin_check(c tb.Context) error {
         defer resp.Body.Close() 
         json.NewDecoder(resp.Body).Decode(&v) 
 	c.Reply(fmt.Sprint(v)) 
-        if v["bank"] == map[]{
+        if v["bank"] == nil{
            fmt.Println("6")
         }
         return nil
