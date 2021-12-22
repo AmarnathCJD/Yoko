e76e397386ff47ee142b55ac32cd4ae704ef20b7
Amarnath C <41010786@sfcollege.edu>
2021-12-22 20:48:10 +0530

    Update inline.go

--- modules/inline.go
+++ modules/inline.go
@@ -12,7 +12,7 @@ func Gg(c tb.Context) error {
 	client := &http.Client{}
 	url := "https://www.google.com/search?&q=cheems&num=8"
 	request, _ := http.NewRequest("GET", url, nil)
-	request.Header.Set("User-Agent", "[Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)] [Chrome/61.0.3163.100 Safari/537.36]")
+	request.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
 	x, _ := client.Do(request)
 	defer x.Body.Close()
 	b, err := io.ReadAll(x.Body)
@@ -20,6 +20,6 @@ func Gg(c tb.Context) error {
 		fmt.Println(err)
 	}
 	doc := soup.HTMLParse(string(b))
-	fmt.Println(doc.Find("div"))
+	fmt.Println(doc.Find("div", "class"))
         return nil
 }
