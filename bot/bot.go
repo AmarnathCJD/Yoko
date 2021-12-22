582e9f5993a20b344e3cec4cab7ef70637d93188
Amarnath C <41010786@sfcollege.edu>
2021-12-22 21:31:48 +0530

    Update bot.go

--- bot/bot.go
+++ bot/bot.go
@@ -8,7 +8,7 @@ func BotInit() tb.Bot {
 	b, _ := tb.NewBot(tb.Settings{
 		URL:         "",
 		Token:       "5050904599:AAEqSus8jD5vTVZzQeTVo4cBc8IwsRha_I0",
-		Poller:      &tb.LongPoller{Timeout: 10, AllowedUpdates: []string{"message", "chat_member"}},
+		Poller:      &tb.LongPoller{Timeout: 10, AllowedUpdates: []string{"message", "chat_member", "inline_query"}},
 		Synchronous: false,
 		Verbose:     false,
 		ParseMode:   "HTML",
