f1b65226f16f162170b262ab0d44673ae7bfc719
Amarnath C <41010786@sfcollege.edu>
2021-12-23 19:29:32 +0530

    Update cmds.go

--- modules/cmds.go
+++ modules/cmds.go
@@ -45,6 +45,6 @@ func RegHandlers() {
 	bot.Bot.Handle("/sban", Ban)
 	bot.Bot.Handle("/dban", Ban)
 	// inline.go
-	bot.Bot.Handle(tb.OnQuery, gsearch_inline)
+	bot.Bot.Handle(tb.OnQuery, ud_inline)
 	bot.Bot.Handle(tb.OnQuery, gsearch_inline)
 }
