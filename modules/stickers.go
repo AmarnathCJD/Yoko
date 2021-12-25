12f3cffbb279f067aeeb755273091960b4eeecc1
Amarnath C <41010786@sfcollege.edu>
2021-12-25 14:47:42 +0530

    Update stickers.go

--- modules/stickers.go
+++ modules/stickers.go
@@ -10,9 +10,7 @@ func AddSticker(c tb.Context) error {
 	pack, _ := db.Get_user_pack(c.Sender().ID)
         fmt.Println(c.Message().Sticker)
 	if !pack {
-                name := fmt.Sprintf("%d_by_Yoko_Robot", c.Sender().ID)
-                fmt.Println(name)
-		err := c.Bot().CreateStickerSet(c.Sender(), tb.StickerSet{Name: name, Title: "hd baby" , Stickers: []tb.Sticker{*c.Message().ReplyTo.Sticker}, PNG: &c.Message().ReplyTo.Sticker.File})
+		err := c.Bot().CreateStickerSet(c.Sender(), tb.StickerSet{Name: fmt.Sprintf("a%d_%d_by_Yoko_Robot", c.Sender().ID, 1), Title: "hd baby" , Stickers: []tb.Sticker{*c.Message().ReplyTo.Sticker}, PNG: &c.Message().ReplyTo.Sticker.File})
 		fmt.Println(err)
 	}
 	return nil
