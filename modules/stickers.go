a2024ce3a426cccdb52c67f29e02c6abf13ffb23
Amarnath C <41010786@sfcollege.edu>
2021-12-25 13:27:15 +0530

    Update stickers.go

--- modules/stickers.go
+++ modules/stickers.go
@@ -1,6 +1,10 @@
 package modules
 
-import "fmt"
+import (
+	"fmt"
+        db "github.com/amarnathcjd/yoko/modules/db"
+	tb "gopkg.in/tucnak/telebot.v3"
+)
 
 func AddSticker(c tb.Context) error {
 	x := db.get_user_pack(c.Sender.ID)
