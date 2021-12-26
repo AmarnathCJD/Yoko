de39b15d113e7e10dd56606c28bf14b471370cc4
Amarnath C <41010786@sfcollege.edu>
2021-12-26 13:15:59 +0530

    Update stickers.go

--- modules/stickers.go
+++ modules/stickers.go
@@ -8,9 +8,18 @@ import (
 
 func AddSticker(c tb.Context) error {
 	pack, _ := db.Get_user_pack(c.Sender().ID)
+        Emoji := "😙"
+        if c.Message().Payload != string(""){
+           Emoji = c.Message().Payload
+        }
 	if !pack {
-		err := c.Bot().CreateStickerSet(c.Sender(), tb.StickerSet{Name: fmt.Sprintf("b%d_%d_by_Yoko_Robot", c.Sender().ID, 1), Title: fmt.Sprintf("%s's kang pack", c.Sender().FirstName) , Stickers: []tb.Sticker{*c.Message().ReplyTo.Sticker}, PNG: &c.Message().ReplyTo.Sticker.File, Emojis: "😙"})
-		fmt.Println(err)
+                Name := fmt.Sprintf("c%d_%d_by_Yoko_Robot", c.Sender().ID, 1)
+		err := c.Bot().CreateStickerSet(c.Sender(), tb.StickerSet{Name: Name, Title: fmt.Sprintf("%s's kang pack", c.Sender().FirstName) , Stickers: []tb.Sticker{*c.Message().ReplyTo.Sticker}, PNG: &c.Message().ReplyTo.Sticker.File, Emojis: Emoji})
+		if err == nil {
+                   db.Add_sticker(c.Sender().ID, Name)
+                   c.Reply(fmt.Sprintf("Sticker successfully added to <b><a href='http://t.me/addstickers/%s'>Pack</a></b>\nEmoji is: %s", Name, Emoji))
+                } else {
+                   c.Reply(err.Error())
 	}
 	return nil
 }
