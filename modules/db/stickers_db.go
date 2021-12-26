fa4d4c309c0b4c27b6dcd3d919fcbca5a60f5630
AmarnathCJD <amarnathcharichilil@gmail.com>
2021-12-26 21:28:27 +0530

    gg

--- modules/db/stickers_db.go
+++ modules/db/stickers_db.go
@@ -20,24 +20,23 @@ func Add_sticker(user_id int64, name string) {
 		s.Decode(&stick)
 		packs := stick["packs"].(bson.A)
 		packs = append(packs, bson.M{"name": name, "count": 1})
-                stickers.UpdateOne(context.TODO(), filter, bson.D{{"$set", bson.D{{"packs", packs}}}}, opts)
+		stickers.UpdateOne(context.TODO(), filter, bson.D{{"$set", bson.D{{"packs", packs}}}}, opts)
 	}
 
 }
 
-func Get_user_pack(user_id int64) (bool, int) {
+func Get_user_pack(user_id int64) (bool, int32, string) {
 	filter := bson.M{"user_id": user_id}
 	s := stickers.FindOne(context.TODO(), filter)
 	if s.Err() != nil {
-		return false, 0
+		return false, 0, ""
 	} else {
 		var stick bson.M
 		s.Decode(&stick)
 		packs := stick["packs"].(bson.A)
-		return true, packs[len(packs)-1].(bson.M)["count"].(int)
+		return true, packs[len(packs)-1].(bson.M)["count"].(int32), packs[len(packs)-1].(bson.M)["name"].(string)
 	}
 }
-
 
 func Update_count(user_id int64, name string) {
 	filter := bson.M{"user_id": user_id}
--- modules/stickers.go
+++ modules/stickers.go
@@ -8,20 +8,44 @@ import (
 )
 
 func AddSticker(c tb.Context) error {
-	pack, _ := db.Get_user_pack(c.Sender().ID)
-	fmt.Println(8)
+	pack, count, name := db.Get_user_pack(c.Sender().ID)
 	Emoji := "😙"
 	if c.Message().Payload != string("") {
 		Emoji = c.Message().Payload
 	}
+	if c.Message().ReplyTo.Photo != nil {
+		c.Reply("sticker file can only be valid wepb files.")
+		return nil
+	} else if c.Message().ReplyTo.Sticker == nil {
+		c.Reply("Yeah, I can't kang that.")
+		return nil
+	}
 	if !pack {
-		Name := fmt.Sprintf("c%d_%d_by_Yoko_Robot", c.Sender().ID, 1)
-		err := c.Bot().CreateStickerSet(c.Sender(), tb.StickerSet{Name: Name, Title: fmt.Sprintf("%s's kang pack", c.Sender().FirstName), Stickers: []tb.Sticker{*c.Message().ReplyTo.Sticker}, PNG: &c.Message().ReplyTo.Sticker.File, Emojis: Emoji})
+		Name := fmt.Sprintf("d%d_%d_by_Yoko_Robot", c.Sender().ID, 1)
+		err := c.Bot().CreateStickerSet(c.Sender(), tb.StickerSet{Name: Name, Title: fmt.Sprintf("%s's kang pack", c.Sender().FirstName), Stickers: []tb.Sticker{*c.Message().Sticker}, PNG: &c.Message().ReplyTo.Sticker.File, Emojis: Emoji})
 		if err == nil {
 			db.Add_sticker(c.Sender().ID, Name)
 			c.Reply(fmt.Sprintf("Sticker successfully added to <b><a href='http://t.me/addstickers/%s'>Pack</a></b>\nEmoji is: %s", Name, Emoji))
 		} else {
 			c.Reply(err.Error())
+		}
+	} else if count <= 120 {
+		stickerset, _ := c.Bot().StickerSet(name)
+		err := c.Bot().AddSticker(c.Sender(), tb.StickerSet{Name: name, Title: stickerset.Title, Stickers: stickerset.Stickers, PNG: &c.Message().ReplyTo.Sticker.File, Emojis: Emoji})
+		if err != nil {
+			c.Reply(err.Error())
+		} else {
+			c.Reply(fmt.Sprintf("Sticker successfully added to <b><a href='http://t.me/addstickers/%s'>Pack</a></b>\nEmoji is: %s", stickerset.Name, Emoji))
+			db.Update_count(c.Sender().ID, stickerset.Name)
+		}
+	} else {
+		Name := fmt.Sprintf("d%d_%d_by_Yoko_Robot", c.Sender().ID, count)
+		err := c.Bot().CreateStickerSet(c.Sender(), tb.StickerSet{Name: Name, Title: fmt.Sprintf("%s's kang pack", c.Sender().FirstName), Stickers: []tb.Sticker{*c.Message().ReplyTo.Sticker}, PNG: &c.Message().ReplyTo.Sticker.File, Emojis: Emoji})
+		if err != nil {
+			c.Reply(err.Error())
+		} else {
+			c.Reply(fmt.Sprintf("Sticker successfully added to <b><a href='http://t.me/addstickers/%s'>Pack</a></b>\nEmoji is: %s", Name, Emoji))
+			db.Add_sticker(c.Sender().ID, Name)
 		}
 	}
 	return nil
