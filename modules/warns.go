16f157c7f674f811f205e640b6ab5242c20e3f6b
Amarnath C <41010786@sfcollege.edu>
2021-12-28 17:59:41 +0530

    Update warns.go

--- modules/warns.go
+++ modules/warns.go
@@ -2,27 +2,32 @@ package modules
 
 import (
 	"strings"
-
+        "fmt"
 	db "github.com/amarnathcjd/yoko/modules/db"
 	tb "gopkg.in/tucnak/telebot.v3"
 )
 
-func WARN(c tb.Context) {
+func WARN(c tb.Context) error {
 	cmd := strings.SplitN(c.Message().Text, " ", 2)[0]
 	if cmd == "/dwarn" && !c.Message().IsReply() {
 		c.Reply("You have to reply to a message to delete it and warn the user.")
 		return nil
 	}
 	user, extra := get_user(c.Message())
-	if user.ID == 6 {
+	if user.ID == int64(6) {
 		c.Reply("Do you really think I can do that to myself <b>:p</b>")
 		return nil
 	}
-	p := c.Bot().ChatMemberOf(c.Chat(), user)
+	p, err := c.Bot().ChatMemberOf(c.Chat(), user)
+        if err != nil {
+           c.Reply(err.Error())
+           return nil
+        }
 	if p.Role != "member" {
 		c.Reply("✨ I'm not going to warn an admin!")
 		return nil
 	}
 	exceeded := db.Warn_user(c.Chat().ID, user.ID, extra)
         fmt.Println(exceeded)
+        return nil
 }
