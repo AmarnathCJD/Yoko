f7deab57a33891429a444803481d7cc4efc80ae8
Amarnath C <41010786@sfcollege.edu>
2021-12-29 13:01:23 +0530

    Update func.go

--- modules/func.go
+++ modules/func.go
@@ -30,7 +30,7 @@ func parse_message(m *tb.Message) (string, string, []string) {
 		} else if len(args) == 2 {
 			if m.ReplyTo.Text != string("") {
 				note, name := m.ReplyTo.Text, args[1]
-                                fmt.Println(m.EntityText())
+                                fmt.Println(m.Text)
 				note += buttons
 				return name, note, []string{file_id, file_type}
 			} else {
