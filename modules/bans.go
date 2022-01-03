4dc68a1ade9c2216c53fa1b2f24686bd464a8717
Amarnath C <amarnathcharichilil@gmail.com>
2022-01-03 07:52:57 +0530

    Update bans.go

--- modules/bans.go
+++ modules/bans.go
@@ -3,6 +3,7 @@ package modules
 import (
 	"fmt"
 	"strings"
+        "time"
 
 	tb "gopkg.in/tucnak/telebot.v3"
 )
@@ -13,10 +14,6 @@ func Ban(c tb.Context) error {
 		return nil
 	}
 	m := c.Message()
-	if m.Private() {
-		c.Reply("This command is for groups.")
-		return nil
-	}
 	user, xtra := get_user(m)
 	if user == nil {
 		return nil
