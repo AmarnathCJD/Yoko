b9db3898d087fb6970e6d9f3c6c50be1ab6329be
Amarnath C <amarnathcharichilil@gmail.com>
2022-01-08 13:37:15 +0530

    Update misc.go

--- modules/misc.go
+++ modules/misc.go
@@ -329,7 +329,7 @@ func Paste(c tb.Context) error {
 	}
 	var body mapType
 	json.NewDecoder(resp.Body).Decode(&body)
-        sel.Inline(sel.Row(sel.URL("View Paste", fmt.Sprintf("https://nekobin.com/%s", body["key"].(string)))))
-	c.Reply(fmt.Sprintf("Pasted to <b><a href='https://nekobin.com/%s'>NekoBin</a></b>.", body["key"].(string)), sel)
+        sel.Inline(sel.Row(sel.URL("View Paste", fmt.Sprintf("https://nekobin.com/%s", body["result"].(map[string]interface{})["key"].(string)))))
+	c.Reply(fmt.Sprintf("Pasted to <b><a href='https://nekobin.com/%s'>NekoBin</a></b>.", body["result"].(map[string]interface{})["key"].(string)), sel)
 	return nil
 }
