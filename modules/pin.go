94ce70cdee1d4e38125979cbd2d59f6d7a34bb41
Amarnath C <amarnathcharichilil@gmail.com>
2022-01-10 20:32:50 +0530

    Update pin.go

--- modules/pin.go
+++ modules/pin.go
@@ -38,7 +38,7 @@ func pin_message(c tb.Context) error {
 	} else {
 		c.Bot().Pin(c.Message().ReplyTo, tb.Silent)
 	}
-	c.Reply(fmt.Sprintf("I have pinned <a href='t.me/c/%d/%d'>this message</a>", c.Message().ReplyTo.Chat.ID, c.Message().ReplyTo.ID))
+	c.Reply(fmt.Sprintf("I have pinned <a href='t.me/c/%d/%d'>this message</a>", strings.ReplaceAll(strconv.Itoa(int(c.Message().ReplyTo.Chat.ID)), "-100", ""), c.Message().ReplyTo.ID))
 	return nil
 }
 
@@ -50,7 +50,7 @@ func pinned_msg(c tb.Context) error {
     c.Reply("There are no pinned messages in this chat.")
     return nil
  } else {
-    chat_id := strings.ReplaceAll(strconv.Itoa(c.Chat().ID), "-100", "")
+    chat_id := strings.ReplaceAll(strconv.Itoa(int(c.Chat().ID)), "-100", "")
     c.Reply(fmt.Sprintf("The pinned message in %s is <b><a href='https://t.me/%s/%s'>Here</a></b>.", c.Chat().Title, chat_id, strconv.Itoa(pinned.ID)))
     return nil
  }
