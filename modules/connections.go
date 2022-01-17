35dc6f45f2809b757782cb3d744ca371accdef28
Amarnath C <amarnathcharichilil@gmail.com>
2022-01-17 18:47:43 +0530

    Update connections.go

--- modules/connections.go
+++ modules/connections.go
@@ -5,7 +5,7 @@ tb "gopkg.in/tucnak/telebot.v3
 )
 
 func Connect_chat(c tb.Context) error {
-if !c.Message().Private{
+if !c.Message().Private(){
 sel.Inline(sel.Row(sel.URL("Connect to chat", fmt.Sprintf("http://t.me/yoko_robot?start=connect_%d", c.Chat().ID))))
 c.Reply("Tap the following button to connect to this chat in PM", sel)
 }
