9c11a9ba06f5fbdb90c078fd4f433f7253f61104
Amarnath C <amarnathcharichilil@gmail.com>
2022-01-17 18:57:14 +0530

    Update start.go

--- modules/start.go
+++ modules/start.go
@@ -26,8 +26,8 @@ func Start(c tb.Context) error {
 		menu.Inline(
 			menu.Row(menu.URL("Support", "t.me/roseloverx_support"), menu.URL("Updates", "t.me/roseloverx_support")),
 			menu.Row(menu.Data("Commands", "help_menu")),
-			menu.Row(menu.URL("Add me to your group", "https://t.me/Aiko_Robot?startgroup=true")))
-		b.Send(m.Sender, "Hey there! I am <b>Aiko</b>.\nIm an Anime themed Group Management Bot, feel free to add me to your groups!", menu)
+			menu.Row(menu.URL("Add me to your group", "https://t.me/Yoko_Robot?startgroup=true")))
+		b.Send(m.Sender, "Hey there! I am <b>Yoko</b>.\nIm an Anime themed Group Management Bot, feel free to add me to your groups!", menu)
 		return nil
 	}
 	b.Reply(m, "Hey I'm Alive.")
