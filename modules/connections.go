57fa5f954b944575060997718d1c322277fe0b04
Amarnath C <amarnathcharichilil@gmail.com>
2022-01-17 19:11:21 +0530

    Update connections.go

--- modules/connections.go
+++ modules/connections.go
@@ -9,7 +9,7 @@ import (
 
 func Connect_chat(c tb.Context) error {
 	if !c.Message().Private() {
-		sel.Inline(sel.Row(sel.URL("Connect to chat", fmt.Sprintf("http://t.me/yoko_robot?start=connect_%d", c.Chat().ID))))
+		sel.Inline(sel.Row(sel.URL("Connect to chat", fmt.Sprintf("t.me/yoko_robot?start=connect_%d", c.Chat().ID))))
 		c.Reply("Tap the following button to connect to this chat in PM", sel)
 	} else if c.Message().Payload == string("") {
 		c.Reply("I need a chat id to connect to!")
