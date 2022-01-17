2d528b1936fa752ba912ba1ca34736bbbd344940
Amarnath C <amarnathcharichilil@gmail.com>
2022-01-17 19:14:59 +0530

    Update start.go

--- modules/start.go
+++ modules/start.go
@@ -28,7 +28,7 @@ func Start(c tb.Context) error {
 	if m.Private() {
 		menu.Inline(
 			menu.Row(menu.URL("Support", "t.me/roseloverx_support"), menu.URL("Updates", "t.me/roseloverx_support")),
-			menu.Row(menu.Data("Commands", "help_menu")),
+			menu.Row(menu.Data("Commands", "back_button")),
 			menu.Row(menu.URL("Add me to your group", "https://t.me/Yoko_Robot?startgroup=true")))
 		b.Send(m.Sender, "Hey there! I am <b>Yoko</b>.\nIm an Anime themed Group Management Bot, feel free to add me to your groups!", menu)
 		return nil
