55b40f86f499526b61ff44b7e110ef1e9b10ed52
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-01-17 13:21:49 +0000

    RoseLoverX: auto fixes

--- modules/connections.go
+++ modules/connections.go
@@ -1,14 +1,14 @@
 package modules
 
 import (
-tb "gopkg.in/tucnak/telebot.v3"
-"fmt"
+	"fmt"
+	tb "gopkg.in/tucnak/telebot.v3"
 )
 
 func Connect_chat(c tb.Context) error {
-if !c.Message().Private(){
-sel.Inline(sel.Row(sel.URL("Connect to chat", fmt.Sprintf("http://t.me/yoko_robot?start=connect_%d", c.Chat().ID))))
-c.Reply("Tap the following button to connect to this chat in PM", sel)
-}
-return nil
+	if !c.Message().Private() {
+		sel.Inline(sel.Row(sel.URL("Connect to chat", fmt.Sprintf("http://t.me/yoko_robot?start=connect_%d", c.Chat().ID))))
+		c.Reply("Tap the following button to connect to this chat in PM", sel)
+	}
+	return nil
 }
