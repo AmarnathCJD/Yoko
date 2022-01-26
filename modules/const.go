2eb720c6a720dd62513781153cffbd7d1ced9c0c
Amarnath C <amarnathcharichilil@gmail.com>
2022-01-26 12:45:12 +0530

    Update const.go

--- modules/const.go
+++ modules/const.go
@@ -4,7 +4,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-var BOT_ID = int64(5050904599)
+var BOT_ID = int64(5181620060)
 
 var notes_help = "✨ Here is the help for **Notes:**\n**Command for Members**\n**->** `/get notename`: get the note with this notename\n**-** #notename: same as /get\n**->** `/notes`: list all saved notes in this chat\n**Command for Admins**\n**->** `/save notename notedata`: saves notedata as a note with name notename, reply to a message or document to save it\n**->** `/clear notename`: clear note with this name\n**->** `/privatenote on/yes/off/no`: whether or not to send the note in PM. Write del besides on/off to delete hashtag message on group.\n**Note**\n **-** Only admins can use This module\n **-** To save a document (like photo, audio, etc.), reply to a document or media then type /save\n **-** Need help for parsing text? Check /markdownhelp\nSave data for future users with notes!\nNotes are great to save random tidbits of information; a phone number, a nice gif, a funny picture - anything!\nAlso you can save a text/document with buttons, you can even save it in here."
 
@@ -13,7 +13,7 @@ var help = bson.M{"notes": notes_help}
 var PLUGIN_LIST = []string{"admin", "bans", "chatbot", "feds", "greetings", "inline", "lock", "misc", "notes", "pin", "stickers", "warns"}
 
 var help_caption = `
-Hey!, My name is Yoko.
+Hey!, My name is Mika.
 I am a group management bot, here to help you get around and keep the order in your groups!
 I have lots of handy features.
 So what are you waiting for?
