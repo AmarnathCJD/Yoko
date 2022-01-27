e58b2856de6aa10af957d40d109e74331c54138a
Amarnath C <amarnathcharichilil@gmail.com>
2022-01-27 07:18:29 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"log"
-
+        "os"
+        "log"
 	mod "github.com/amarnathcjd/yoko/modules"
 
 	bot "github.com/amarnathcjd/yoko/bot"
@@ -10,6 +11,8 @@ import (
 
 func main() {
 	mod.RegisterHandlers()
+        file, _ := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+        log.SetOutput(file)
 	log.Print("Bot started")
 	bot.Bot.Start()
 }
