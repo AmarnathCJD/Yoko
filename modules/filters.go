61ef2ef8e2c92978196e42f36365092771553d2f
Amarnath C <amarnathcharichilil@gmail.com>
2022-01-30 09:44:57 +0530

    Update filters.go

--- modules/filters.go
+++ modules/filters.go
@@ -99,7 +99,7 @@ func FilterEvent(c tb.Context) bool {
 		pattern := `( |^|[^\w])(?i)` + x + `( |$|[^\w])`
 		if match, _ := regexp.Match(pattern, []byte(c.Text())); match {
 			filter := db.Get_filter(c.Chat().ID, x)
-			unparse_message(filter["file"], ParseText(filter["text"].(string), c), c.Message())
+			unparse_message(filter["file"], ParseString(filter["text"].(string), c), c.Message())
 			return true
 		}
 	}
