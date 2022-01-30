3efb01d638c25426b2925b0fa266244a7fad46c1
Amarnath C <amarnathcharichilil@gmail.com>
2022-01-30 11:25:45 +0530

    Update func.go

--- modules/func.go
+++ modules/func.go
@@ -497,9 +497,8 @@ func ParseString(t string, c tb.Context) string {
 
 	}
         if strings.Contains(t, "{rules}"){
-strings.ReplaceAll("{rules}", fmt.Sprintf("[Rules](buttonurl://rules|%d)", c.Chat().ID)
-
-}
+               strings.ReplaceAll("{rules}", fmt.Sprintf("[Rules](buttonurl://rules|%d)", c.Chat().ID))
+        }
 	first := c.Sender().FirstName
 	last := c.Sender().LastName
 	fullname := first + " " + last
