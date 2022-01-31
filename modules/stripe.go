1e5f7678d56bba102dfb5961aa94438e54da0195
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-01-31 15:42:24 +0000

    RoseLoverX: auto fixes

--- modules/stripe.go
+++ modules/stripe.go
@@ -154,17 +154,17 @@ func confirm(id string, s string, cc string, year string, month string, cvc stri
 		if m, ok := e["message"]; ok {
 			msg = m.(string)
 		}
-                if n, ok := e["next_action"] ; ok {
-if p, ok := n.(map[string]interface{})["type"]; ok {
- if p.(string) == "use_stripe_sdk"{
-   code = "charge_failed"
-   dcode = "vbv"
-   msg = "Failed to charge your card."
-}
+		if n, ok := e["next_action"]; ok {
+			if p, ok := n.(map[string]interface{})["type"]; ok {
+				if p.(string) == "use_stripe_sdk" {
+					code = "charge_failed"
+					dcode = "vbv"
+					msg = "Failed to charge your card."
+				}
 
-}
+			}
 
-}
+		}
 	}
 	return code, dcode, msg
 }
@@ -180,8 +180,8 @@ func StripeRs(cc string, month string, year string, cvc string) string {
 	if code == "card_declined" {
 		emoji = "❌"
 	} else if code == "incorrect_number" {
-emoji = "❌"
-}
+		emoji = "❌"
+	}
 	if dcode != string("") {
 		dcode = fmt.Sprintf("\n<b>➤ Dcode:</b> %s", dcode)
 	}
