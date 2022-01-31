f7ab12277f052507b7752393d50d01b50f8833a2
Amarnath C <amarnathcharichilil@gmail.com>
2022-01-31 20:12:08 +0530

    Create stripe.go

--- modules/stripe.go
+++ modules/stripe.go
@@ -44,7 +44,7 @@ func get_id() string {
 
 }
 
-func create_intent() string {
+func create_intent() (string, string) {
 	client := &http.Client{}
 	var data = strings.NewReader(`{"amount":7300,"currency":"inr","account":"us","metaData":{"designation":"general","donation_type":"One-Time","amount_without_fee":73,"utm_source":null,"utm_medium":null,"utm_campaign":null,"utm_content":null,"utm_term":null,"int_source":"yaqeeninstitute.org","int_campaign":"general","int_content":"main_nav_donate_button"},"testMode":false,"statementDescriptorSuffix":"One Time"}`)
 	req, err := http.NewRequest("POST", "https://yaqeeninstitute.ca/api/v1/donate/stripe/create-payment-intent", data)
@@ -72,7 +72,7 @@ func create_intent() string {
 	defer resp.Body.Close()
 	var m map[string]string
 	json.NewDecoder(resp.Body).Decode(&m)
-	return m["id"]
+	return m["id"], m["clientSecret"]
 }
 
 func update_id(id string) string {
@@ -110,10 +110,10 @@ func update_id(id string) string {
 	return m["clientSecret"]
 }
 
-func confirm(id string, cc string, year string, month string, cvc string) (string, string, string) {
+func confirm(id string, s string, cc string, year string, month string, cvc string) (string, string, string) {
 	client := &http.Client{}
-	client_secret := update_id(id)
-	var data = strings.NewReader(`receipt_email=devnull%40yaqeeninstitute.org&payment_method_data[type]=card&payment_method_data[metadata][designation]=general&payment_method_data[metadata][donation_type]=One-Time&payment_method_data[metadata][amount_without_fee]=73&payment_method_data[metadata][int_source]=yaqeeninstitute.org&payment_method_data[metadata][int_campaign]=general&payment_method_data[metadata][int_content]=main_nav_donate_button&payment_method_data[metadata][payment_method]=cc&payment_method_data[billing_details][name]=Jenna+M+Ortega&payment_method_data[billing_details][phone]=%2B1+9402197942&payment_method_data[billing_details][email]=amarnathc%40outlook.in&payment_method_data[billing_details][address][line1]=394+Olen+Thomas+Drive&payment_method_data[billing_details][address][line2]=&payment_method_data[billing_details][address][city]=Crowell&payment_method_data[billing_details][address][country]=US&payment_method_data[billing_details][address][postal_code]=79227&payment_method_data[billing_details][address][state]=TX&payment_method_data[card][number]=` + cc + `&payment_method_data[card][cvc]=` + cvc + `&payment_method_data[card][exp_month]=` + month + `&payment_method_data[card][exp_year]=` + year + `&payment_method_data[guid]=73f5a9dc-4f9d-4102-9f39-112d2bc87189f08893&payment_method_data[muid]=c2fe2add-ecb3-4320-915b-a926ca22c4a498e15e&payment_method_data[sid]=6c0ab791-b55b-4969-b556-9aca59adccd76700f0&payment_method_data[pasted_fields]=number&payment_method_data[payment_user_agent]=stripe.js%2F7050ff317%3B+stripe-js-v3%2F7050ff317&payment_method_data[time_on_page]=1967550&expected_payment_method_type=card&use_stripe_sdk=true&webauthn_uvpa_available=false&spc_eligible=false&key=pk_live_tGDwiUeDEcgPGTyb51bqDNG8&client_secret=` + client_secret)
+	
+	var data = strings.NewReader(`receipt_email=devnull%40yaqeeninstitute.org&payment_method_data[type]=card&payment_method_data[metadata][designation]=general&payment_method_data[metadata][donation_type]=One-Time&payment_method_data[metadata][amount_without_fee]=73&payment_method_data[metadata][int_source]=yaqeeninstitute.org&payment_method_data[metadata][int_campaign]=general&payment_method_data[metadata][int_content]=main_nav_donate_button&payment_method_data[metadata][payment_method]=cc&payment_method_data[billing_details][name]=Jenna+M+Ortega&payment_method_data[billing_details][phone]=%2B1+9402197942&payment_method_data[billing_details][email]=amarnathc%40outlook.in&payment_method_data[billing_details][address][line1]=394+Olen+Thomas+Drive&payment_method_data[billing_details][address][line2]=&payment_method_data[billing_details][address][city]=Crowell&payment_method_data[billing_details][address][country]=US&payment_method_data[billing_details][address][postal_code]=79227&payment_method_data[billing_details][address][state]=TX&payment_method_data[card][number]=` + cc + `&payment_method_data[card][cvc]=` + cvc + `&payment_method_data[card][exp_month]=` + month + `&payment_method_data[card][exp_year]=` + year + `&payment_method_data[guid]=73f5a9dc-4f9d-4102-9f39-112d2bc87189f08893&payment_method_data[muid]=c2fe2add-ecb3-4320-915b-a926ca22c4a498e15e&payment_method_data[sid]=6c0ab791-b55b-4969-b556-9aca59adccd76700f0&payment_method_data[pasted_fields]=number&payment_method_data[payment_user_agent]=stripe.js%2F7050ff317%3B+stripe-js-v3%2F7050ff317&payment_method_data[time_on_page]=1967550&expected_payment_method_type=card&use_stripe_sdk=true&webauthn_uvpa_available=false&spc_eligible=false&key=pk_live_tGDwiUeDEcgPGTyb51bqDNG8&client_secret=` + s)
 	req, err := http.NewRequest("POST", fmt.Sprintf("https://api.stripe.com/v1/payment_intents/%s/confirm", id), data)
 	if err != nil {
 		log.Fatal(err)
@@ -158,9 +158,8 @@ func confirm(id string, cc string, year string, month string, cvc string) (strin
 }
 
 func StripeRs(cc string, month string, year string, cvc string) string {
-	id := create_intent()
-	fmt.Println(id)
-	code, dcode, msg := confirm(id, cc, month, year, cvc)
+	id, sec := create_intent()
+	code, dcode, msg := confirm(id, sec, cc, month, year, cvc)
 	emoji := "❌"
 	if code != "card_declined" {
 		emoji = "✅"
