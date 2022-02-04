19b2b347bee618f9cc1c5126c22774efb8dd16d0
Amarnath C <amarnathcharichilil@gmail.com>
2022-02-04 19:38:30 +0530

    Update anon.go

--- modules/anon.go
+++ modules/anon.go
@@ -3,7 +3,7 @@ package modules
 
 import "gopkg.in/tucnak/telebot.v3"
 
-ANON = make(map[int]func(tb.Context) error)
+var ANON = make(map[int]func(tb.Context) error)
 
 func AnonAdmin(next tb.HandlerFunc) tb.HandlerFunc {
 fmt.Println("Hmm")
