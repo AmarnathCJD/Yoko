4ea9a7b98fa210fdb5ece3736cfb796e2a608410
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-02-04 14:15:03 +0000

    RoseLoverX: auto fixes

--- modules/anon.go
+++ modules/anon.go
@@ -1,23 +1,19 @@
 package modules
 
-
 import tb "gopkg.in/tucnak/telebot.v3"
 import "fmt"
 
 var ANON = make(map[int]func(tb.Context) error)
 
 type Update struct {
-Id int
-Func func(tb.Context) error
-Right string
-
-
+	Id    int
+	Func  func(tb.Context) error
+	Right string
 }
 
-
 func AnonAdmin(next tb.HandlerFunc) tb.HandlerFunc {
-fmt.Println("Hmm")
-return nil
+	fmt.Println("Hmm")
+	return nil
 
 }
 
