7ca039f544224990ed49e00498853fa2a3d86852
Amarnath C <amarnathcharichilil@gmail.com>
2022-02-04 22:04:43 +0530

    Update anon.go

--- modules/anon.go
+++ modules/anon.go
@@ -47,13 +47,13 @@ func AnonCB(c tb.Context) error {
 	b, err := json.Marshal(update.C.Sender())
 	s := c.Bot().NewContext(tb.Update{ID: update.C.Message().ID,
 		Message: &tb.Message{
-			Sender:  c.Message().Sender,
+			Sender:  c.Sender(),
 			Chat:    update.C.Message().Chat,
 			Payload: update.C.Message().Payload,
 			Text:    update.C.Message().Text,
 		},
 	})
-	fmt.Println(string(b), s.Sender())
+	fmt.Println(string(b), s.Sender(), s.Chat())
 	return nil
 }
 
