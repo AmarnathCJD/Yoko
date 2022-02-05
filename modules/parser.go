c70b8801bbc25c2a7579b875893d1c1ed8af4943
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-02-05 12:15:14 +0000

    RoseLoverX: auto fixes

--- modules/parser.go
+++ modules/parser.go
@@ -3,8 +3,8 @@ package modules
 import (
 	"fmt"
 	tb "gopkg.in/tucnak/telebot.v3"
-"regexp"
-"strings"
+	"regexp"
+	"strings"
 )
 
 var HyperLink = regexp.MustCompile(`\[(.*?)\]\((.*?)\)`)
@@ -27,19 +27,19 @@ func ParseMD(c tb.Context) string {
 			text = string(text[:offset+cor]) + "<code>" + string(text[offset+cor:offset+cor+length]) + "</code>" + string(text[offset+cor+length:])
 			cor += 13
 		} else if x.Type == tb.EntityUnderline {
-text = string(text[:offset+cor]) + "<code>" + string(text[offset+cor:offset+cor+length]) + "</code>" + string(text[offset+cor+length:])
+			text = string(text[:offset+cor]) + "<code>" + string(text[offset+cor:offset+cor+length]) + "</code>" + string(text[offset+cor+length:])
 			cor += 7
-} else if x.Type == tb.EntityItalic {
-text = string(text[:offset+cor]) + "<code>" + string(text[offset+cor:offset+cor+length]) + "</code>" + string(text[offset+cor+length:])
+		} else if x.Type == tb.EntityItalic {
+			text = string(text[:offset+cor]) + "<code>" + string(text[offset+cor:offset+cor+length]) + "</code>" + string(text[offset+cor+length:])
 			cor += 7
-}
+		}
 	}
-        Links := HyperLink.FindAllStringSubmatch(text, -1)
+	Links := HyperLink.FindAllStringSubmatch(text, -1)
 	if Links != nil {
 		for _, x := range Links {
-                        if strings.Contains(x[2], "buttonurl") {
-continue
-}
+			if strings.Contains(x[2], "buttonurl") {
+				continue
+			}
 			text = strings.Replace(text, x[0], fmt.Sprintf("<a href='%s'>%s</a>", x[1], x[2]), 1)
 			fmt.Println(text)
 		}
