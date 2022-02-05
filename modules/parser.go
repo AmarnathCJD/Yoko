a11ffcc308dea115a264153a33c7ac7d0879ca44
Amarnath C <amarnathcharichilil@gmail.com>
2022-02-05 17:42:46 +0530

    Update parser.go

--- modules/parser.go
+++ modules/parser.go
@@ -3,7 +3,10 @@ package modules
 import (
 	"fmt"
 	tb "gopkg.in/tucnak/telebot.v3"
+"regexp"
 )
+
+var HyperLink = regexp.MustCompile(`\[(.*?)\]\((.*?)\)`)
 
 func PARSET(c tb.Context) error {
 
@@ -23,12 +26,22 @@ func ParseMD(c tb.Context) string {
 			text = string(text[:offset+cor]) + "<code>" + string(text[offset+cor:offset+cor+length]) + "</code>" + string(text[offset+cor+length:])
 			cor += 13
 		} else if x.Type == tb.EntityUnderline {
-text = text = string(text[:offset+cor]) + "<code>" + string(text[offset+cor:offset+cor+length]) + "</code>" + string(text[offset+cor+length:])
+text = string(text[:offset+cor]) + "<code>" + string(text[offset+cor:offset+cor+length]) + "</code>" + string(text[offset+cor+length:])
 			cor += 7
 } else if x.Type == tb.EntityItalic {
 text = string(text[:offset+cor]) + "<code>" + string(text[offset+cor:offset+cor+length]) + "</code>" + string(text[offset+cor+length:])
 			cor += 7
 }
+	}
+        Links := HyperLink.FindAllStringSubmatch(text, -1)
+	if Links != nil {
+		for _, x := range f {
+                        if strings.Contains(x[2], "buttonurl") {
+continue
+}
+			text = strings.Replace(text, x[0], fmt.Sprintf("<a href='%s'>%s</a>", x[1], x[2]), 1)
+			fmt.Println(text)
+		}
 	}
 	fmt.Println(text)
 	return text
