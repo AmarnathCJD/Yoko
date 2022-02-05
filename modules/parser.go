94b894d843df0a878be4734684b0008f1c40fdf6
Amarnath C <amarnathcharichilil@gmail.com>
2022-02-05 16:37:17 +0530

    Update parser.go

--- modules/parser.go
+++ modules/parser.go
@@ -6,12 +6,18 @@ import (
 )
 
 func PARSET(c tb.Context) error {
-	fmt.Println(ParseMD(c))
-	return nil
+	
+	return c.Reply(ParseMD(c))
 
 }
 
 func ParseMD(c tb.Context) string {
-	return fmt.Sprint(c.Message().Entities)
+        text := c.Message().ReplyTo.Text
+        for i, x := range c.Message().Entites{
+if x.Type == tb.EntityBold {
+offset, length := x.Offset, x.Length
+ text = string(text[:offset]) + "<b>" + string(text[offset:offset+length]) + "</b>" + string(text[offset+length:])
+}
+	return fmt.Sprint(text)
 
 }
