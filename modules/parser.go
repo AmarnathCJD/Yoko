b1e013d9db376b9eaa0bba1935983579d50348d7
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-02-05 11:25:26 +0000

    RoseLoverX: auto fixes

--- modules/parser.go
+++ modules/parser.go
@@ -13,16 +13,16 @@ func PARSET(c tb.Context) error {
 
 func ParseMD(c tb.Context) string {
 	text := c.Message().ReplyTo.Text
-        cor := 0
+	cor := 0
 	for _, x := range c.Message().ReplyTo.Entities {
-offset, length := x.Offset, x.Length
+		offset, length := x.Offset, x.Length
 		if x.Type == tb.EntityBold {
 			text = string(text[:offset+cor]) + "<b>" + string(text[offset+cor:offset+cor+length]) + "</b>" + string(text[offset+cor+length:])
-                        cor += 6
+			cor += 6
 		} else if x.Type == tb.EntityCode {
-text = string(text[:offset+cor]) + "<code>" + string(text[offset+cor:offset+cor+length]) + "</code>" + string(text[offset+cor+length:])
-                        cor += 12
-}
+			text = string(text[:offset+cor]) + "<code>" + string(text[offset+cor:offset+cor+length]) + "</code>" + string(text[offset+cor+length:])
+			cor += 12
+		}
 	}
 	fmt.Println(text)
 	return text
