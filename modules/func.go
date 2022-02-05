47f26f31d8defa11e0664ff5a46af0e79f2b3019
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-02-05 16:34:13 +0000

    RoseLoverX: auto fixes

--- modules/func.go
+++ modules/func.go
@@ -96,7 +96,7 @@ func get_file(m *tb.Message) (string, string) {
 
 func unparse_message(file interface{}, note string, m *tb.Message, p bool) {
 	text, buttons := button_parser(note)
-        fmt.Println("H8")
+	fmt.Println("H8")
 	if file != nil && len(file.(map[string]interface{})) != 0 {
 		id, f := file.(bson.A)[0].(string), file.(bson.A)[1].(string)
 		if f == "document" {
@@ -125,7 +125,7 @@ func unparse_message(file interface{}, note string, m *tb.Message, p bool) {
 			b.Reply(m, file, &tb.SendOptions{ReplyMarkup: buttons, DisableWebPagePreview: p})
 		}
 	} else {
-                fmt.Println("H")
+		fmt.Println("H")
 		ms, err := b.Reply(m, text, &tb.SendOptions{ReplyMarkup: buttons, DisableWebPagePreview: p})
 		fmt.Println(err, ms)
 	}
