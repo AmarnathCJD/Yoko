33b906c17edc6a26eb69b1b8f1dc76de8887741e
Amarnath C <amarnathcharichilil@gmail.com>
2022-02-06 17:15:39 +0530

    Update notes.go

--- modules/notes.go
+++ modules/notes.go
@@ -98,7 +98,6 @@ func OnTextHandler(c tb.Context) error {
 }
 
 func Hash_note(c tb.Context) error {
-	fmt.Println("#hash")
 	args := strings.SplitN(c.Message().Text, "#", 2)
 	note := db.Get_note(c.Message().Chat.ID, args[1])
 	if note == nil {
