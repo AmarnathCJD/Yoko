9eeec0ff3b5dd7cee91c3dd3ce4c9b5058f9a172
Amarnath C <amarnathcharichilil@gmail.com>
2022-02-08 21:44:39 +0530

    Update notes.go

--- modules/notes.go
+++ modules/notes.go
@@ -82,6 +82,7 @@ func Gnote(c tb.Context) error {
 
 func OnTextHandler(c tb.Context) error {
 	FLOOD_EV(c)
+        Chat_bot(c)
 	if err, ok := FilterEvent(c); ok {
 		return err
 	}
@@ -93,7 +94,7 @@ func OnTextHandler(c tb.Context) error {
 	if afk := AFK(c); afk {
 		return nil
 	}
-	Chat_bot(c)
+	
 	return nil
 }
 
