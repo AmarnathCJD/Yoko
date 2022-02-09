f98eb1b1eb012f9570388b4d2697527be610ee3a
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-02-09 02:07:14 +0000

    RoseLoverX: auto fixes

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 )
 
 func main() {
-        file, _ := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-        log.Print("Bot started")
+	file, _ := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	log.Print("Bot started")
 	log.SetOutput(file)
 	mod.RegisterHandlers()
 	// logging
