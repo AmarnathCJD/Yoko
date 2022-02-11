2bce15742ddbf8722925b54271a2954079e38383
Amarnath C <amarnathcharichilil@gmail.com>
2022-02-11 15:53:23 +0530

    Update misc.go

--- modules/misc.go
+++ modules/misc.go
@@ -573,6 +573,5 @@ func Music(c tb.Context) error {
 		Performer: vid.Author,
 		FileName:  vid.Title,
 		Duration:  int(duration.Seconds()),
-		Thumbnail: &tb.Photo{File: tb.FromURL(vid.Thumbnails[0].URL)},
 	})
 }
