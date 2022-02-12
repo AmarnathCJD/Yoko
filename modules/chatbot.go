8046804e28136f664f565ece087b422aa5dd7d68
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-02-12 09:40:30 +0000

    RoseLoverX: auto fixes

--- modules/chatbot.go
+++ modules/chatbot.go
@@ -52,7 +52,7 @@ func Chat_bot(c tb.Context) error {
 				f := strings.SplitN(media[0][0], "<image>", -1)
 				fl := f[len(f)-1]
 				file = strings.ReplaceAll(strings.ReplaceAll(fl, "</image>", ""), "</image>", "")
-                                
+
 			}
 			if strings.HasSuffix(file, "jpg") || strings.HasSuffix(file, "png") {
 				c.Reply(&tb.Photo{File: tb.FromURL(file)})
@@ -61,11 +61,11 @@ func Chat_bot(c tb.Context) error {
 			}
 		}
 	}
-var message string
+	var message string
 	for _, m := range media {
-message = strings.Replace(message, m[0], "", -1)
-}
-        
+		message = strings.Replace(message, m[0], "", -1)
+	}
+
 	if strings.Contains(message, "<split>") {
 		message = strings.ReplaceAll(strings.ReplaceAll(message, "<split>", ""), "</split>", "")
 	}
