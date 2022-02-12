87a825d2a4d3fe57d7539af96e5d8838c7bd63f3
AmarnathCJD <amarnathcharichilil@gmail.com>
2022-02-12 11:46:31 +0530

    gg

--- modules/afk.go
+++ modules/afk.go
@@ -56,7 +56,7 @@ func AFK(c tb.Context) bool {
 					reason = fmt.Sprintf(",\n<b>Reason:</b> %s", r.(string))
 				}
 				since := get_readable_time(time.Unix(int64(a["time"].(int64)), 0), time.Now())
-				if err := c.Reply(fmt.Sprintf("<b>%s</b> is AFK !\nLast Seen: %s ago.%s", a["fname"].(string), since.String(), reason)); err != nil {
+				if err := c.Reply(fmt.Sprintf("<b>%s</b> is AFK !\nLast Seen: %s ago.%s", a["fname"].(string), since.Truncate(time.Second).String(), reason)); err != nil {
 					fmt.Println(err)
 				}
 				return true
