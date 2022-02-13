a0be0dca6f4bb1b159a63a1e40dcac67b5067f9d
Amarnath C <amarnathcharichilil@gmail.com>
2022-02-13 12:08:30 +0530

    Update stickers_db.go

--- modules/db/stickers_db.go
+++ modules/db/stickers_db.go
@@ -8,18 +8,18 @@ import (
 
 var stickers = database.Collection("stick")
 
-func Add_sticker(user_id int64, name string, _type string) {
+func Add_sticker(user_id int64, name string, title string, _type string) {
 	filter := bson.M{"user_id": user_id, "type": _type}
 	s := stickers.FindOne(context.TODO(), filter)
 	if s.Err() != nil {
 		var packs bson.A
-		packs = append(packs, bson.M{"name": name, "count": 1})
+		packs = append(packs, bson.M{"name": name, "count": 1, "title": title})
 		stickers.UpdateOne(context.TODO(), filter, bson.D{{Key: "$set", Value: bson.D{{Key: "packs", Value: packs}}}}, opts)
 	} else {
 		var stick bson.M
 		s.Decode(&stick)
 		packs := stick["packs"].(bson.A)
-		packs = append(packs, bson.M{"name": name, "count": 1})
+		packs = append(packs, bson.M{"name": name, "count": 1, "title": title})
 		stickers.UpdateOne(context.TODO(), filter, bson.D{{Key: "$set", Value: bson.D{{Key: "packs", Value: packs}}}}, opts)
 	}
 
@@ -38,7 +38,7 @@ func Get_user_pack(user_id int64, _type string) (bool, int32, string) {
 	}
 }
 
-func Get_user_packs(user_id int64) []string {
+func Get_user_packs(user_id int64) map[string]string {
 	var files []bson.M
 	filter := bson.M{"user_id": user_id}
 	r, _ := filters.Find(context.TODO(), filter)
@@ -46,10 +46,10 @@ func Get_user_packs(user_id int64) []string {
 	if len(files) == 0 {
 		return nil
 	} else {
-		var Names []string
+		var Names = make(map[string]string{})
 		for _, y := range files {
 			for _, x := range y["packs"].(bson.A) {
-				Names = append(Names, x.(bson.M)["name"].(string))
+				Names[x.(bson.M)["name"].(string)] = x.(bson.M)["title"].(string)
 			}
 		}
 		return Names
