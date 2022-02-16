77b9b0d8ec70f6719af936ee50c70b408fd2cc9c
Amarnath C <amarnathcharichilil@gmail.com>
2022-02-16 07:30:09 +0530

    Update bot.go

--- bot/bot.go
+++ bot/bot.go
@@ -2,15 +2,23 @@ package bot
 
 import (
 	"fmt"
-
+"os"
 	tb "gopkg.in/telebot.v3"
 )
+
 
 func BotInit() tb.Bot {
 	b, _ := tb.NewBot(tb.Settings{
 		URL:         "",
 		Token:       "5181620060:AAF6FCOW9M6tbUPPtLs_b2UkXKGz7ksaggo",
-		Poller:      &tb.LongPoller{Timeout: 10, AllowedUpdates: []string{"message", "chat_member", "inline_query", "callback_query"}},
+		Poller:      &tb.Webhook{
+  Listen: os.Getenv("PORT"),
+  Endpoint: &tb.WebhookEndpoint{
+    PublicURL: "https://golang-yoko.herokuapp.com/",
+  },
+  MaxConnections: 100,
+  AllowedUpdates: []string{"message", "chat_member", "inline_query", "callback_query"},
+}
 		Synchronous: false,
 		Verbose:     false,
 		ParseMode:   "HTML",
@@ -19,6 +27,7 @@ func BotInit() tb.Bot {
 			fmt.Println(e)
 		},
 	})
+
 	return *b
 }
 
