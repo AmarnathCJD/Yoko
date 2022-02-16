5529ca063efac62db66fbb288b5716613d79a04f
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-02-16 02:04:59 +0000

    RoseLoverX: auto fixes

--- bot/bot.go
+++ bot/bot.go
@@ -5,19 +5,18 @@ import (
 	tb "gopkg.in/telebot.v3"
 )
 
-
 func BotInit() tb.Bot {
 	b, _ := tb.NewBot(tb.Settings{
-		URL:         "",
-		Token:       "5181620060:AAF6FCOW9M6tbUPPtLs_b2UkXKGz7ksaggo",
-		Poller:      &tb.Webhook{
-  Listen: "https://golang-yoko.herokuapp.com",
-  Endpoint: &tb.WebhookEndpoint{
-    PublicURL: "https://golang-yoko.herokuapp.com/",
-  },
-  MaxConnections: 100,
-  AllowedUpdates: []string{"message", "chat_member", "inline_query", "callback_query"},
-},
+		URL:   "",
+		Token: "5181620060:AAF6FCOW9M6tbUPPtLs_b2UkXKGz7ksaggo",
+		Poller: &tb.Webhook{
+			Listen: "https://golang-yoko.herokuapp.com",
+			Endpoint: &tb.WebhookEndpoint{
+				PublicURL: "https://golang-yoko.herokuapp.com/",
+			},
+			MaxConnections: 100,
+			AllowedUpdates: []string{"message", "chat_member", "inline_query", "callback_query"},
+		},
 		Synchronous: false,
 		Verbose:     false,
 		ParseMode:   "HTML",
