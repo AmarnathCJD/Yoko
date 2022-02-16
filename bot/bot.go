285fa26e3b81499b70fea114c354354ab7589fe5
Amarnath C <amarnathcharichilil@gmail.com>
2022-02-16 07:32:43 +0530

    Update bot.go

--- bot/bot.go
+++ bot/bot.go
@@ -12,13 +12,13 @@ func BotInit() tb.Bot {
 		URL:         "",
 		Token:       "5181620060:AAF6FCOW9M6tbUPPtLs_b2UkXKGz7ksaggo",
 		Poller:      &tb.Webhook{
-  Listen: os.Getenv("PORT"),
+  Listen: "https://golang-yoko.herokuapp.com",
   Endpoint: &tb.WebhookEndpoint{
     PublicURL: "https://golang-yoko.herokuapp.com/",
   },
   MaxConnections: 100,
   AllowedUpdates: []string{"message", "chat_member", "inline_query", "callback_query"},
-}
+},
 		Synchronous: false,
 		Verbose:     false,
 		ParseMode:   "HTML",
