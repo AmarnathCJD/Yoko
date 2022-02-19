b78cefe8d4553f4480bcd5e132c83d961dc35208
AmarnathCJD <amanathcharichilil@gmail.com>
2022-02-19 00:22:34 -0800

    dd

--- modules/cmds.go
+++ modules/cmds.go
@@ -148,7 +148,7 @@ func GatherHandlers() map[string]HANDLE {
 	HANDLERS["remsudo"] = HANDLE{FUNC: RemoveSudo}
 	HANDLERS["logs"] = HANDLE{FUNC: Logs}
 	HANDLERS["ping"] = HANDLE{FUNC: Ping}
-	HANDLERS["test"] = HANDLE{FUNC: PARSET}
+	HANDLERS["pong"] = HANDLE{FUNC: Te}
 	return HANDLERS
 
 }
