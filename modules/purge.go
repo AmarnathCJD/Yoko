b83ccaa19eb9bb40ae6a9a3f861cc9c70897b773
Amarnath C <amarnathcharichilil@gmail.com>
2022-02-25 15:33:06 +0530

    Update purge.go

--- modules/purge.go
+++ modules/purge.go
@@ -7,19 +7,10 @@ import (
 )
 
 func Purge(c tb.Context) error {
-	Args := c.Message().Payload
-	Reply := c.Message().ReplyTo
-	var lt = Reply.ID
-	if isInt(Args) {
-		ArgsInt, _ := strconv.Atoi(Args)
-		if ArgsInt > 0 {
-			lt = ArgsInt
-		}
-	}
-	if Reply == nil {
+	if !c.Message().IsReply() {
 		return c.Reply("Reply to a message to show me where to purge from.")
 	}
-	for i := Reply.ID; i <= c.Message().ID-(Reply.ID-lt); i++ {
+	for i := Reply.ID; i <= c.Message().ID; i++ {
 		ID := i
 		go func() {
 			c.Bot().Delete(&tb.Message{ID: ID, Chat: c.Message().Chat})
@@ -27,4 +18,13 @@ func Purge(c tb.Context) error {
 	}
 	c.Delete()
 	return c.Send("Purge complete.")
+}
+
+func Delete(c tb.Context) error {
+ 
+if !c.Message().IsReply() {
+		return c.Reply("Reply to a message to delete it.")
+	}
+c.Bot().Delete(c.Message().ReplyTo)
+
 }
