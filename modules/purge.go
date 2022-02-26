36e827534446200acdee947f6678c44d5cd45fc4
Amarnath C <amarnathcharichilil@gmail.com>
2022-02-26 07:32:23 +0530

    Update purge.go

--- modules/purge.go
+++ modules/purge.go
@@ -8,13 +8,19 @@ func Purge(c tb.Context) error {
 	if !c.Message().IsReply() {
 		return c.Reply("Reply to a message to show me where to purge from.")
 	}
+var CanDelete = true
 	for i := c.Message().ReplyTo.ID; i <= c.Message().ID; i++ {
 		ID := i
 		go func() {
 			err := c.Bot().Delete(&tb.Message{ID: ID, Chat: c.Message().Chat})
-			check(err)
+			if err.Error() == "telegram: message can't be deleted (400)"{
+CanDelete = false
+}
 		}()
 	}
+        if !CanDelete {
+return c.Reply("I can't delete messages in this chat! Give me admin and message deleting rights first.")
+}
 	c.Delete()
 	return c.Send("Purge complete.")
 }
