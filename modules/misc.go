3f564ab2aaca67c8efc284900356f73616f4d174
Amarnath C <amarnathcharichilil@gmail.com>
2022-02-28 21:22:11 +0530

    Update misc.go

--- modules/misc.go
+++ modules/misc.go
@@ -628,7 +628,7 @@ func DogeSticker(c tb.Context) error {
 	dc.SetRGB(1, 1, 1)
 	dc.Clear()
 	dc.SetRGB(0, 0, 0)
-	if err := dc.LoadFontFace("./modules/assets/Swiss 721 Black Extended BT.ttf", float64(85*(5/len(Args)))); err != nil {
+	if err := dc.LoadFontFace("./modules/assets/Swiss 721 Black Extended BT.ttf", GetDimension(len(Args))); err != nil {
 		check(err)
 	}
 	dc.DrawStringAnchored(Args, (461/2)-40, (512/3*3/4)-20, 0.5, 0.5)
@@ -638,4 +638,16 @@ func DogeSticker(c tb.Context) error {
 	dc.Clip()
 	dc.SavePNG("out.webp")
 	return c.Reply(&tb.Sticker{File: tb.File{FileLocal: "out.webp"}})
+}
+
+func GetDimension(i int) float64 {
+if i <= 5 {
+return float64(85)
+} else {
+i = i - 5
+for x; x = 0; x<=i; x++ {
+i = i-(i*(1/85))
+}
+}
+return float64 (i)
 }
