8042f2e583b5543cb45c7d2d4b3d596c31a01616
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-03-01 05:12:08 +0000

    RoseLoverX: auto fixes

--- modules/misc.go
+++ modules/misc.go
@@ -617,8 +617,8 @@ func Music(c tb.Context) error {
 		Performer: vid.Author,
 		FileName:  vid.Title,
 		Duration:  int(duration.Seconds()),
-                Thumbnail: &tb.Photo{File: tb.FromURL(vid.Thumbnails[0].URL)},
-                Caption: vid.Title,
+		Thumbnail: &tb.Photo{File: tb.FromURL(vid.Thumbnails[0].URL)},
+		Caption:   vid.Title,
 	})
 }
 
