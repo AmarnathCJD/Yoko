37b34c33b0b0c4464a05db3069d54fd4dd7587a0
Amarnath C <amarnathcharichilil@gmail.com>
2022-03-07 10:53:49 +0530

    Update func.go

--- modules/func.go
+++ modules/func.go
@@ -601,15 +601,14 @@ func ByteCount(b int64) string {
 		float64(b)/float64(div), "kMGTPE"[exp])
 }
 
-func SearchVideos(query string, limit int) {
+func SearchVideos(query string, limit int) YoutubeResult {
 	var YouTubeApi = "https://www.youtube.com/youtubei/v1/search?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"
 	var Payload = strings.NewReader(`{"context":{"client":{"clientName":"WEB","clientVersion":"2.20220303.06.01"}},"query":"` + query + `"}`)
 	req, _ := http.NewRequest("POST", YouTubeApi, Payload)
 	req.Header.Set("content-type", "application/json")
 	resp, _ := Client.Do(req)
 	defer resp.Body.Close()
-	var r YoutubeResult
+	var result YoutubeResult
 	json.NewDecoder(resp.Body).Decode(&r)
-	b, _ := json.Marshal(r)
-	log.Println(string(b))
+	return result
 }
