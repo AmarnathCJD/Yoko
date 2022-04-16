b97a2e667c6b9149ab9f0d2410a0d72a6375bdab
Amarnath C <amarnathcharichilil@gmail.com>
2022-04-16 22:49:49 +0530

    Update bot.go

--- bot/bot.go
+++ bot/bot.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	tb "gopkg.in/telebot.v3"
 	"log"
+        "os"
 )
 
 func BotInit() tb.Bot {
 	b, _ := tb.NewBot(tb.Settings{
 		URL:         "",
-		Token:       "5181620060:AAF6FCOW9M6tbUPPtLs_b2UkXKGz7ksaggo",
+		Token:       os.Getenv("TOKEN"),
 		Poller:      &tb.LongPoller{Timeout: 10, AllowedUpdates: []string{"message", "chat_member", "inline_query", "callback_query"}},
 		Synchronous: false,
 		Verbose:     false,
