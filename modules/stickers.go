2f613ec1eaa27d5111f591a1e16544a154469e47
Amarnath C <amarnathcharichilil@gmail.com>
2022-05-18 22:19:28 +0530

    Update stickers.go

--- modules/stickers.go
+++ modules/stickers.go
@@ -56,11 +56,11 @@ func AddSticker(c tb.Context) error {
 	}
 	if Sticker.Video || Sticker.Animated {
 		var Ext = "webm"
-		var PrePre = "vidp"
+		var PrePre = "vidpas"
 		var Prefix = "Video"
 		if Sticker.Animated {
 			Ext = "tgs"
-			PrePre = "tgsp"
+			PrePre = "tgspas"
 			Prefix = "Animated"
 		}
 		Pack, Count = db.GetPack(c.Sender().ID, Ext)
@@ -102,7 +102,7 @@ func AddSticker(c tb.Context) error {
 	}
 	title := fmt.Sprintf("%s's kang pack", c.Sender().FirstName)
 	if Pack.Name == "" {
-		Name := fmt.Sprintf("pngs%d_%d_by_missmikabot", c.Sender().ID, 1)
+		Name := fmt.Sprintf("pngdf%d_%d_by_missmikabot", c.Sender().ID, 1)
 		err := c.Bot().CreateStickerSet(c.Sender(), tb.StickerSet{Name: Name, Title: fmt.Sprintf("%s's kang pack", c.Sender().FirstName), Stickers: []tb.Sticker{*Sticker}, PNG: &Sticker.File, Emojis: Emoji, Video: false, Animated: false})
 		if err == nil {
 			db.AddSticker(c.Sender().ID, Name, title, "png")
@@ -122,7 +122,7 @@ func AddSticker(c tb.Context) error {
 			db.UpdateCount(c.Sender().ID, "png")
 		}
 	} else {
-		Name := fmt.Sprintf("pngs%d_%d_by_missmikabot", c.Sender().ID, Count)
+		Name := fmt.Sprintf("pngdf%d_%d_by_missmikabot", c.Sender().ID, Count)
 		err := c.Bot().CreateStickerSet(c.Sender(), tb.StickerSet{Name: Name, Title: fmt.Sprintf("%s's kang pack", c.Sender().FirstName), Stickers: []tb.Sticker{*Sticker}, PNG: &Sticker.File, Emojis: Emoji})
 		if err != nil {
 			c.Reply(err.Error())
