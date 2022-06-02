1001db0354eee84939ef497713b070978e8ba9e7
Amarnath C <amarnathcharichilil@gmail.com>
2022-06-02 19:46:13 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,12 +6,9 @@ import (
 
 	bot "github.com/amarnathcjd/yoko/bot"
 	mod "github.com/amarnathcjd/yoko/modules"
-	db "github.com/amarnathcjd/yoko/modules/db"
 )
 
 func main() {
-	fmt.Println(db.GetFiltersFromDB(100))
-	return
 	log.Print("Bot Started.")
 	mod.RegisterHandlers()
 	bot.Bot.Start()
