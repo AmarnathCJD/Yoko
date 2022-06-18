11c4a2e630b01d6ffa947a0c8c54e09662979b67
Amarnath C <amarnathcharichilil@gmail.com>
2022-06-18 19:42:15 +0530

    Create album.go

---
+++ modules/album.go
@@ -0,0 +1,63 @@
+package modules
+
+import (
+"time"
+)
+
+func OnMediaHandler(c tb.Context) error {
+	if c.Message().AlbumID != string("") {
+		return c.Reply(fmt.Sprint(time.Now().Unix()))
+	}
+	return nil
+}
+
+
+func copyAlbum(c) {
+
+}
+
+type Album struct {
+	AlbumID   string
+	TimeStamp int64
+	Files     []string
+}
+
+var a []Album
+
+func main() {
+	data := "100"
+	_time := time.Now().Unix()
+	AppendAlbum(data, _time, "1")
+	AppendAlbum(data, _time, "2")
+	AppendAlbum(data, _time, "3")
+	time.Sleep(time.Second * 1)
+	AppendAlbum("200", time.Now().Unix(), "4")
+}
+
+func AlbumExist(id string) (bool, int) {
+	for i, b := range a {
+		if b.AlbumID == id {
+			return true, i
+		}
+	}
+	return false, 0
+}
+
+func AppendAlbum(data string, _time int64, f string) {
+	e, i := AlbumExist(data)
+	if !e {
+		a = append(a, Album{data, _time, []string{f}})
+		_, i = AlbumExist(data)
+		go func() {
+			time.Sleep(time.Second * 1)
+			fmt.Println("Hui")
+			SendAlbum(i)
+		}()
+	} else {
+		a[i].Files = append(a[i].Files, f)
+	}
+}
+
+func SendAlbum(_id int) {
+	fmt.Println(a[_id])
+}
